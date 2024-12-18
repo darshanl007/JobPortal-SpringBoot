7248940bbd0f00720f295cdb7ecf18e42fdda13e
Darshan L <darshanl1254@gmail.com>
2024-12-18 22:33:36 +0530

    JobPortal Project Setup

---
+++ src/test/java/org/dars/job_portal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dars.job_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
